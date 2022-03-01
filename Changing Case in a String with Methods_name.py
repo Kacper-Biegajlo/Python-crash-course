8ae808213ea6d6cc50edfb1e919e1febd36a9eef
BlueCodeMaster3000 <99098000+BlueCodeMaster3000@users.noreply.github.com>
2022-03-01 17:49:59 +0100

    Delete Changing Case in a String with Methods_name.py

--- Changing Case in a String with Methods_name.py
+++
@@ -1,6 +0,0 @@
-name = "ada lovelace"
-print(name.title())              # name in tittle method
-
-name = "Ada Lovelace"
-print(name.upper())              # name with all uppercase
-print(name.lower())              # name with all lowercase
